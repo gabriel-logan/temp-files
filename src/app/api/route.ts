9ac2829f8898e22979d27b160358a7e721777750
gabriel-logan <gabrielcoutolima@protonmail.ch>
2025-11-28 16:42:42 -0300

    refactor: replace global cached file with a Map for better cache management

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,8 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
 
-// Cache temporário global
-let cachedFile: { buffer: Buffer; filename: string; type: string } | null =
-  null;
+// Cache
+const fileCache = new Map<
+  string,
+  { buffer: Buffer; filename: string; type: string }
+>();
 
 // Função auxiliar para ler body multipart (upload)
 async function parseMultipart(req: NextRequest) {
@@ -23,8 +25,8 @@ export async function POST(req: NextRequest) {
   try {
     const { buffer, filename, type } = await parseMultipart(req);
 
-    // Armazena em cache
-    cachedFile = { buffer, filename, type };
+    // Armazena no Map
+    fileCache.set("file", { buffer, filename, type });
 
     return NextResponse.json({
       message: "Arquivo enviado com sucesso!",
@@ -32,35 +34,39 @@ export async function POST(req: NextRequest) {
       type,
       size: buffer.length,
     });
-  } catch (err: { message: string } | unknown) {
-    return NextResponse.json(
-      { error: (err as { message: string }).message },
-      { status: 400 },
-    );
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      return NextResponse.json({ error: err.message }, { status: 400 });
+    }
+
+    return NextResponse.json({ error: "Erro inesperado" }, { status: 500 });
   }
 }
 
 // ----------------- LIMPAR CACHE -----------------
 export async function PUT() {
-  cachedFile = null;
+  fileCache.delete("file");
+
   return NextResponse.json({ message: "Cache limpo com sucesso!" });
 }
 
 // ----------------- DOWNLOAD -----------------
 export async function GET() {
-  if (!cachedFile) {
+  const cached = fileCache.get("file");
+
+  if (!cached) {
     return NextResponse.json(
       { error: "Nenhum arquivo em cache" },
       { status: 404 },
     );
   }
 
-  return new NextResponse(new Uint8Array(cachedFile.buffer), {
+  return new NextResponse(new Uint8Array(cached.buffer), {
     headers: {
-      "Content-Type": cachedFile.type,
-      "Content-Disposition": `attachment; filename="${cachedFile.filename}"`,
+      "Content-Type": cached.type,
+      "Content-Disposition": `attachment; filename="${cached.filename}"`,
     },
   });
 }
 
-export { cachedFile };
+export { fileCache };
