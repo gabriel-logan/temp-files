f8eaa7d5234af40a019e14894a91a651cef270ce
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2025-11-28 19:25:23 -0300

    feat: add link to navigate to Files page from Home

--- src/app/files/page.tsx
+++ src/app/files/page.tsx
@@ -1,10 +1,25 @@
+import { filesCache } from "@/lib/cache/file-cache";
+import Link from "next/link";
+
 export default function FilesPage() {
+  const files = Array.from(filesCache.values()).flat();
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <h1 className="text-4xl font-bold">Files Page</h1>
       <p className="mt-4 text-lg text-gray-600 dark:text-gray-300">
         This is where you can manage your files.
       </p>
+
+      <ul className="mt-6 space-y-4">
+        {files.map((file) => (
+          <Link key={file.fileId} href={`/api/file/${file.fileId}`}>
+            <li className="w-full max-w-md rounded-lg border p-4">
+              <h2 className="text-2xl font-semibold">{file.filename}</h2>
+            </li>
+          </Link>
+        ))}
+      </ul>
     </main>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <div className="before:bg-gradient-radial after:bg-gradient-conic relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:lg:h-[360px] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40">
+      <div className="before:bg-gradient-radial after:bg-gradient-conic relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-60 after:translate-x-1/3 after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:lg:h-[360px] before:dark:bg-linear-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40">
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
           src="/file.svg"
@@ -18,6 +19,10 @@ export default function Home() {
       <p className="mt-4 text-lg text-gray-600 dark:text-gray-300">
         A simple file sharing app
       </p>
+
+      <Link href="/files" className="mt-6 text-blue-600 hover:underline">
+        Go to Files
+      </Link>
     </main>
   );
 }
